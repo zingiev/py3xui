4d9a5b18f3bfcfeebc6692b5fd1a15d62c76e1ea
zingiev <m@zingiev.ru>
2025-05-18 00:49:19 +0300

    1

---
+++ pyxray/db.py
@@ -0,0 +1,83 @@
+import sqlite3
+from typing import Optional, List, Dict
+
+
+class DB:
+
+    def __init__(self, db_name: str = 'pyxray.session'):
+        self.connect = sqlite3.connect(db_name)
+        self.connect.row_factory = sqlite3.Row  # Позволяет возвращать словари
+        self.cursor = self.connect.cursor()
+        self._create_table()
+
+    def _create_table(self) -> None:
+        with self.connect:
+            self.cursor.execute(
+                '''
+                CREATE TABLE IF NOT EXISTS session (
+                    id INTEGER PRIMARY KEY,
+                    name TEXT NOT NULL,
+                    value TEXT NOT NULL,
+                    domain TEXT NOT NULL,
+                    path TEXT NOT NULL,
+                    secure TEXT NOT NULL
+                )
+                '''
+            )
+
+    def insert_data(self, **cookie: str) -> None:
+        with self.connect:
+            self.cursor.execute(
+                '''
+                INSERT INTO session (name, value, domain, path, secure)
+                VALUES (:name, :value, :domain, :path, :secure)
+                ''',
+                cookie
+            )
+
+    def update_data(self, **cookie: str) -> None:
+        with self.connect:
+            self.cursor.execute(
+                '''
+                UPDATE session
+                SET name = :name,
+                    value = :value,
+                    path = :path,
+                    secure = :secure
+                WHERE domain = :domain AND name = :name
+                ''',
+                cookie
+            )
+
+    def exists_data(self, domain: str) -> bool:
+        self.cursor.execute(
+            '''SELECT 1
+               FROM session
+               WHERE domain = ?
+               LIMIT 1''', (domain,)
+        )
+        return self.cursor.fetchone() is not None
+
+    def exists_cookie(self, domain: str, name: str) -> bool:
+        self.cursor.execute(
+            '''SELECT 1
+               FROM session
+               WHERE domain = ?
+               AND name = ?
+               LIMIT 1''', (domain, name)
+        )
+        return self.cursor.fetchone() is not None
+
+    def get_cookies_by_domain(self, domain: str) -> List[Dict[str, str]]:
+        self.cursor.execute(
+            '''SELECT name, value, domain, path, secure
+               FROM session
+               WHERE domain = ?''', (domain,)
+        )
+        rows = self.cursor.fetchall()
+        return [dict(row) for row in rows]
+
+    def get_all(self) -> List[Dict[str, str]]:
+        self.cursor.execute('SELECT * FROM session')
+        rows = self.cursor.fetchall()
+        return [dict(row) for row in rows]
--- pyxray/pyxray.py
+++ pyxray/pyxray.py
@@ -1,19 +1,11 @@
+import random
+from string import ascii_lowercase, digits
 from http import HTTPStatus
-from dataclasses import dataclass
 from requests import Session
 
 from .db import DB
 
 db = DB()
-
-
-@dataclass
-class Cookies:
-    name: str
-    value: str
-    domain: str
-    path: str
-    secure: bool
 
 
 class Client:
@@ -49,7 +41,8 @@ class Client:
 
     def _store_cookies(self) -> None:
         if not self.session.cookies:
-            raise Exception('No cookies found. Possibly incorrect credentials.')
+            raise Exception(
+                'No cookies found. Possibly incorrect credentials.')
 
         for cookie in self.session.cookies:
             cookie_data = {
@@ -68,7 +61,8 @@ class Client:
     def _load_cookies_from_db(self) -> None:
         cookies = db.get_cookies_by_domain(self.host)
         if not cookies:
-            raise Exception("No cookies found in database. Please login again.")
+            raise Exception(
+                "No cookies found in database. Please login again.")
 
         for c in cookies:
             self.session.cookies.set(
@@ -78,13 +72,82 @@ class Client:
                 path=c['path']
             )
 
-    def inbounds(self) -> dict:
-        url = self._build_url(f'{self.base_url}/list')
+    def get_inbounds_request(self, url: str):
         headers = {'Accept': 'application/json'}
         response = self.session.get(url, headers=headers)
-
         if response.status_code == HTTPStatus.OK:
             self._store_cookies()
             return response.json()
-
         raise Exception(response.reason, response.status_code)
+
+    def inbounds(self) -> dict:
+        url = self._build_url(f'{self.base_url}/list')
+        response = self.get_inbounds_request(url)
+        return response
+
+    def inbound(self, inbound_id: str) -> dict:
+        url = self._build_url(f'{self.base_url}/get/{inbound_id}')
+        response = self.get_inbounds_request(url)
+        return response
+
+    def get_traffics_with_email(self, email: str) -> dict:
+        url = self._build_url(f'{self.base_url}/getClientTraffics/{email}')
+        headers = {'Accept': 'application/json'}
+        response = self.session.get(url, headers=headers)
+        if response.status_code == HTTPStatus.OK:
+            self._store_cookies()
+            return response.json()
+        raise Exception(response.reason, response.status_code)
+
+    def add_inbound(
+        self,
+        name_inbound: str = 'New',
+        enable: bool = True,
+        expiry_time: int = 0,
+        port: int = 55421,
+        protocol: str = 'vless'
+    ):
+        url = self._build_url(f'{self.base_url}/add/')
+        headers = {'Accept': 'application/json'}
+        payload = {
+            "success": 'true',
+            "msg": "Create Successfully",
+            "obj": {
+                "up": 0,
+                "down": 0,
+                "total": 0,
+                "remark": name_inbound,
+                "enable": enable,
+                "expiryTime": expiry_time,
+                "clientStats": 'null',
+                "listen": "",
+                "port": port,
+                "protocol": protocol,
+                "settings": "{\"clients\": [{\"id\": \"b86c0cdc-8a02-4da4-8693-72ba27005587\",\"flow\": \"\",\"email\": \"nt3wz904\",\"limitIp\": 0,\"totalGB\": 0,\"expiryTime\": 0,\"enable\": true,\"tgId\": \"\",\"subId\": \"rqv5zw1ydutamcp0\",\"reset\": 0}],\"decryption\": \"none\",\"fallbacks\": []}",
+                "streamSettings": "{\"network\": \"tcp\",\"security\": \"reality\",\"externalProxy\": [],\"realitySettings\": {\"show\": false,\"xver\": 0,\"dest\": \"yahoo.com:443\",\"serverNames\": [\"yahoo.com\",\"www.yahoo.com\"],\"privateKey\": \"wIc7zBUiTXBGxM7S7wl0nCZ663OAvzTDNqS7-bsxV3A\",\"minClient\": \"\",\"maxClient\": \"\",\"maxTimediff\": 0,\"shortIds\": [\"47595474\",\"7a5e30\",\"810c1efd750030e8\",\"99\",\"9c19c134b8\",\"35fd\",\"2409c639a707b4\",\"c98fc6b39f45\"],\"settings\": {\"publicKey\": \"2UqLjQFhlvLcY7VzaKRotIDQFOgAJe1dYD1njigp9wk\",\"fingerprint\": \"random\",\"serverName\": \"\",\"spiderX\": \"/\"}},\"tcpSettings\": {\"acceptProxyProtocol\": false,\"header\": {\"type\": \"none\"}}}",
+                "tag": "inbound-55421",
+                "sniffing": "{\"enabled\": true,\"destOverride\": [\"http\",\"tls\",\"quic\",\"fakedns\"],\"metadataOnly\": false,\"routeOnly\": false}",
+                "allocate": "{\"strategy\": \"always\",\"refresh\": 5,\"concurrency\": 3}"
+            }
+        }
+        response = self.session.post(url, headers=headers, params=payload)
+        if response.status_code == HTTPStatus.OK:
+            self._store_cookies()
+            return response.json()
+        raise Exception(response.reason, response.status_code)
+
+# dd9ebef2-ce00-47fe-adc3-31d4a13343a5
+
+    def add_user_to_inbound(self, inbound_id: int, email: str = None):
+        if not email:
+            string = ascii_lowercase + digits
+            idx = []
+            gen_length = [8, 4, 4, 4, 12]
+            for i in gen_length:
+                idx.append([random.choice(string) for _ in range(i)])
+            idx = '-'.join([''.join(i) for i in idx])
+        url = self._build_url(f'{self.base_url}/addClient')
+        payload = {
+        "id": inbound_id,
+        "settings": "{\"clients\": [{\"id\": \"bbfad557-28f2-47e5-9f3d-e3c7f539fbda\",\"flow\": \"xtls-rprx-vision\",\"email\": \"dp1plm9lt8\",\"limitIp\": 0,\"totalGB\": 0,\"expiryTime\": 0,\"enable\": true,\"tgId\": \"\",\"subId\": \"2rv0gb458kbfl592\",\"reset\": 0}]}"
+    }
