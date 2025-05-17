800c89d790134c292c32792bd997ea7d5ad37259
zingiev <m@zingiev.ru>
2025-05-17 21:19:03 +0300

    1

--- pyxray/pyxray.py
+++ pyxray/pyxray.py
@@ -1,65 +1,90 @@
-import os
-import json
 from http import HTTPStatus
-
+from dataclasses import dataclass
 from requests import Session
+
+from .db import DB
+
+db = DB()
+
+
+@dataclass
+class Cookies:
+    name: str
+    value: str
+    domain: str
+    path: str
+    secure: bool
 
 
 class Client:
-
-    SESSION_FILE = 'session.json'
-
     def __init__(self, host: str, port: int, web_base_path: str) -> None:
         self.host = host
         self.port = port
         self.web_base_path = web_base_path
         self.base_url = '/panel/api/inbounds'
         self.session = Session()
-        self._load_session_cookies()
 
-    def _save_session_cookies(self) -> None:
-        cookies = [
-            {
-                'name': c.name,
-                'value': c.value,
-                'domain': c.domain,
-                'path': c.path,
-                'secure': c.secure,
-                'expires': c.expires
-            }
-            for c in self.session.cookies
-        ]
-        with open(self.SESSION_FILE, 'w') as f:
-            json.dump(cookies, f, indent=2)
+        if db.exists_data(self.host):
+            self._load_session_cookies()
+        else:
+            self._prompt_login()
 
-    def _load_session_cookies(self) -> None:
-        if not os.path.exists(self.SESSION_FILE):
-            return
+    def _prompt_login(self) -> None:
+        username = input('Username: ')
+        password = input('Password: ')
+        self._login_and_store(username, password)
 
-        with open(self.SESSION_FILE, 'r') as f:
-            cookies = json.load(f)
-            for c in cookies:
-                self.session.cookies.set(
-                    name=c['name'],
-                    value=c['value'],
-                    domain=c['domain'],
-                    path=c['path']
-                )
-
-    def login(self, username: str, password: str) -> dict:
-        url = f'http://{self.host}:{self.port}/{self.web_base_path}/login'
+    def _login_and_store(self, username: str, password: str) -> None:
+        url = self._build_url('login')
         payload = {'username': username, 'password': password}
         response = self.session.post(url, data=payload)
+
         if response.status_code == HTTPStatus.OK:
-            self._save_session_cookies()
-            return response.json()
-        raise Exception(response.reason, response.status_code)
+            self._store_cookies()
+        else:
+            raise Exception(response.reason, response.status_code)
+
+    def _build_url(self, path: str) -> str:
+        return f'http://{self.host}:{self.port}/{self.web_base_path}/{path}'
+
+    def _store_cookies(self) -> None:
+        if not self.session.cookies:
+            raise Exception('No cookies found. Possibly incorrect credentials.')
+
+        for cookie in self.session.cookies:
+            cookie_data = {
+                'name': cookie.name,
+                'value': cookie.value,
+                'domain': cookie.domain,
+                'path': cookie.path,
+                'secure': cookie.secure
+            }
+
+            if db.exists_cookie(self.host, cookie.name):
+                db.update_data(**cookie_data)
+            else:
+                db.insert_data(**cookie_data)
+
+    def _load_session_cookies(self) -> None:
+        cookies = db.get_cookies_by_domain(self.host)
+        if not cookies:
+            raise Exception("No cookies found in database. Please login again.")
+
+        for c in cookies:
+            self.session.cookies.set(
+                name=c["name"],
+                value=c["value"],
+                domain=c["domain"],
+                path=c["path"]
+            )
 
     def inbounds(self) -> dict:
-        url = f'http://{self.host}:{self.port}/{self.web_base_path}{self.base_url}/list'
+        url = f'{self._build_url("panel/api/inbounds/list")}'
         headers = {'Accept': 'application/json'}
         response = self.session.get(url, headers=headers)
+
         if response.status_code == HTTPStatus.OK:
-            self._save_session_cookies()
+            self._store_cookies()
             return response.json()
+
         raise Exception(response.reason, response.status_code)
