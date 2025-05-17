156981df6f74c23598040d4874f652c154e5b7ae
zingiev <m@zingiev.ru>
2025-05-17 21:24:32 +0300

    1

--- pyxray/pyxray.py
+++ pyxray/pyxray.py
@@ -20,21 +20,21 @@ class Client:
     def __init__(self, host: str, port: int, web_base_path: str) -> None:
         self.host = host
         self.port = port
-        self.web_base_path = web_base_path
-        self.base_url = '/panel/api/inbounds'
+        self.web_base_path = web_base_path.strip('/')
+        self.base_url = 'panel/api/inbounds'
         self.session = Session()
 
         if db.exists_data(self.host):
-            self._load_session_cookies()
+            self._load_cookies_from_db()
         else:
             self._prompt_login()
 
     def _prompt_login(self) -> None:
         username = input('Username: ')
         password = input('Password: ')
-        self._login_and_store(username, password)
+        self._login_and_store_cookies(username, password)
 
-    def _login_and_store(self, username: str, password: str) -> None:
+    def _login_and_store_cookies(self, username: str, password: str) -> None:
         url = self._build_url('login')
         payload = {'username': username, 'password': password}
         response = self.session.post(url, data=payload)
@@ -57,29 +57,29 @@ class Client:
                 'value': cookie.value,
                 'domain': cookie.domain,
                 'path': cookie.path,
-                'secure': cookie.secure
+                'secure': str(cookie.secure)
             }
 
-            if db.exists_cookie(self.host, cookie.name):
+            if db.exists_cookie(cookie.domain, cookie.name):
                 db.update_data(**cookie_data)
             else:
                 db.insert_data(**cookie_data)
 
-    def _load_session_cookies(self) -> None:
+    def _load_cookies_from_db(self) -> None:
         cookies = db.get_cookies_by_domain(self.host)
         if not cookies:
             raise Exception("No cookies found in database. Please login again.")
 
         for c in cookies:
             self.session.cookies.set(
-                name=c["name"],
-                value=c["value"],
-                domain=c["domain"],
-                path=c["path"]
+                name=c['name'],
+                value=c['value'],
+                domain=c['domain'],
+                path=c['path']
             )
 
     def inbounds(self) -> dict:
-        url = f'{self._build_url("panel/api/inbounds/list")}'
+        url = self._build_url(f'{self.base_url}/list')
         headers = {'Accept': 'application/json'}
         response = self.session.get(url, headers=headers)
 
